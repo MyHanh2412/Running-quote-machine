fe9e5b3dece6a0e9e4a8f196596eacfdcb3e270a
MyHanh2412 <50241507+MyHanh2412@users.noreply.github.com>
2019-05-03 20:12:35 +0300

    03.05.2019 introduction JS

---
+++ hanh/me.js
@@ -0,0 +1,19 @@
+function Test(){
+    alert("shut up");
+}
+function hannes(){
+    
+    console.log('next');
+}
+function myloop(){
+    var i = 0;
+while (i<3){
+    i++;
+    console.log(i);
+}
+
+
+
+var result = 0;
+   console.log(result)
+}
